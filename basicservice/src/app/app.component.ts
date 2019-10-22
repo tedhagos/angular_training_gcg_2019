58ee5cc8b5e5edc04021d2062fe83079581a16af
ted <tedhagos@gmail.com>
2019-10-22 22:17:06 +0800

    basicservice project

--- basicservice/src/app/app.component.ts
+++ basicservice/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'basicservice';
+  books: any[];
+  
+  constructor(private bookService: BookService){
+  }
+  ngOnInit(): void {
+    this.books = this.bookService.getBooks();
+  }
 }
